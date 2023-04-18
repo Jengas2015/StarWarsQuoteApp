998686c9720d4681e60723fa0ddefcbe0993397a
Jengas2015 <arejang@gmail.com>
2023-04-18 14:34:18 -0500

    set up Mongo DB

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function () {
-    console.log("listening on 3000")
-})
+let dbConnectionStr = process.env.DB_STRING
 
-app.get('/',  (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
 
-app.post('/quotes', (req, res)=> {
-    console.log("hellooooooo!")
-})
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log("Connected to Database")
+        const db = client.db("star-wars-quotes")
+        app.use(express.urlencoded({ extended: true }))
+        app.use(express.json())
+
+        app.listen(3000, function () {
+            console.log("listening on 3000")
+        })
+
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html')
+        })
+
+        app.post('/quotes', (req, res) => {
+            console.log(req.body)
+        })
+    })
+    .catch(console.error)
+    
